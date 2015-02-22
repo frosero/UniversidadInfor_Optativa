7be93fa2d6c5668abc7f2ac582d18b1cc5062154
Felipe Rosero <darkfenixx1@hotmail.com>
2015-02-21 21:42:50 -0500

    Layout Prueba con suma y servicio web

--- app/src/main/java/com/uce/asus/universidadinfor_optativa/UniversidadInfo.java
+++ app/src/main/java/com/uce/asus/universidadinfor_optativa/UniversidadInfo.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import org.ksoap2.serialization.SoapObject;
 
 
 public class UniversidadInfo extends ActionBarActivity {
@@ -14,7 +18,35 @@ public class UniversidadInfo extends ActionBarActivity {
         setContentView(R.layout.activity_universidad_info);
     }
 
+    @Override
+    protected void EnviarOnclick(View v){
 
+
+
+
+
+
+        Thread nt = new Thread(){
+            String res;
+            final EditText Numero1 = (EditText) findViewById(R.id.et_num1);
+            final EditText Numero2 = (EditText) findViewById(R.id.et_num2);
+
+            @Override
+            public void run(){
+                String NAMESPACE ="http://localhost/";
+                String URL="http://localhost:9000/Demo_WS_Android.asmx";
+                String METHOD_NAME="Suma";
+                String SOAP_ACTION="http://localhost/Suma";
+
+                SoapObject request = new SoapObject(NAMESPACE,METHOD_NAME);
+                request.addProperty("Numero1", Integer.parseInt(Numero1.getText().toString()));
+                request.addProperty("Numero2", Integer.parseInt(Numero2.getText().toString()));
+
+
+            }
+
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
